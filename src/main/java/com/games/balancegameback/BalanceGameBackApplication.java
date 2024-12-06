1bc0e4eae760118e209a596334dc0e8762d17f25
cheomuk <pkw9815@naver.com>
2024-12-06 21:29:14 +0900

    First Commit

---
+++ src/main/java/com/games/balancegameback/BalanceGameBackApplication.java
@@ -0,0 +1,13 @@
+package com.games.balancegameback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceGameBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalanceGameBackApplication.class, args);
+    }
+
+}
