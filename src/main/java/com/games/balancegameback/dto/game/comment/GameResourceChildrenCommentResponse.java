71dfb70d403efd5de39ce1c6c85aec1876334959
cheomuk <pkw9815@naver.com>
2025-04-04 01:47:12 +0900

    feat: Anonymize comments and added creator tags based on whether the game's creator is anonymous or not. #63

--- src/main/java/com/games/balancegameback/dto/game/comment/GameResourceChildrenCommentResponse.java
+++ src/main/java/com/games/balancegameback/dto/game/comment/GameResourceChildrenCommentResponse.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Getter
 @Builder
@@ -24,14 +24,17 @@ public class GameResourceChildrenCommentResponse {
     private String nickname;
 
     @Schema(description = "작성 시간")
-    private LocalDateTime createdDateTime;
+    private OffsetDateTime createdDateTime;
 
     @Schema(description = "수정 시간")
-    private LocalDateTime updatedDateTime;
+    private OffsetDateTime updatedDateTime;
 
     @Schema(description = "좋아요 총합")
     private int like;
 
     @Schema(description = "좋아요 클릭 유무")
-    private boolean isLiked;
+    private boolean existsLiked;
+
+    @Schema(description = "작성자 본인 확인")
+    private boolean existsWriter;
 }
