e729517e847e0fd061505515a7b2eed3380e0b99
cheomuk <pkw9815@naver.com>
2025-04-13 22:42:48 +0900

    feat: Add category data.

--- src/main/java/com/games/balancegameback/domain/game/enums/Category.java
+++ src/main/java/com/games/balancegameback/domain/game/enums/Category.java
@@ -1,6 +1,7 @@
 package com.games.balancegameback.domain.game.enums;
 
 public enum Category {
-    // 추후 데이터를 받으면 추가 예정 (써놓은 것들은 예시)
-    FUN, HORROR, HOT, ACTION, ADVENTURE, MYSTERY, FANTASY, THRILLER, SF, DRAMA, ROMANCE, CRIME, SURVIVAL
+    // 추후 데이터를 받으면 추가 예정
+    FUN, HORROR, HOT, ACTION, ADVENTURE, MYSTERY, FANTASY, THRILLER, SF, DRAMA, ROMANCE, CRIME, SURVIVAL,
+    EVERYDAY, FOOD, LOVE, SONG, CELEBRITY, ANIMAL, ANIME, ETC
 }
